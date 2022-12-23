1e3beb42729be7d798f0931884c37e48868a10b5
Tom Lonergan <tomlonergan91@gmail.com>
2022-12-23 02:22:12 +0000

    day 3 done

--- rust/day3/src/main.rs
+++ rust/day3/src/main.rs
@@ -1,28 +1,25 @@
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
-use std::vec;
 
 fn main() {
     let path = Path::new("../../inputs/day3.txt");
+    let part_1_priority: u32 = read_lines(path)
+        .expect("Line buffer failure")
+        .map(Result::unwrap)
+        .map(score_part_1)
+        .sum();
 
-    let mut current_part_1: u32 = 0;
-    let mut current_part_2: u32 = 0;
-    let mut group: Vec<String> = vec![];
-    let lines = read_lines(path).expect("Line buffer failure");
+    let part_2_priority: u32 = read_lines(path)
+        .expect("Line buffer failure")
+        .map(Result::unwrap)
+        .collect::<Vec<String>>()
+        .chunks(3)
+        .map(score_part_2)
+        .sum();
 
-    for line in lines {
-        let line = line.expect("Line read failure");
-        current_part_1 += score_part_1(&line);
-        group.push(line);
-        if group.len() == 3 {
-            current_part_2 += score_part_2(&group);
-            group.clear();
-        }
-    }
-
-    println!("Part 1 score = {current_part_1}");
-    println!("Part 2 score = {current_part_2}");
+    println!("Part 1 score = {part_1_priority}");
+    println!("Part 2 score = {part_2_priority}");
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -33,24 +30,40 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-fn score_part_1(line:&String) -> u32 {
+fn determine_priority(item: char) -> u32 {
+    if item < 'a' {
+        return item as u32 - 38;
+    }
+    return item as u32 - 96;
+}
+
+fn score_part_1(line: String) -> u32 {
     let length = line.chars().count();
-    let (compartment_1, compartment_2) = line.split_at(length/2);
-    for item_1 in compartment_1.chars(){
-        for item_2 in compartment_2.chars(){
+    let (compartment_1, compartment_2) = line.split_at(length / 2);
+    for item_1 in compartment_1.chars() {
+        for item_2 in compartment_2.chars() {
             if item_1 != item_2 {
                 continue;
             }
-            if item_1 < 'a' {
-                return item_1 as u32 - 38;
-            }
-            return  item_1 as u32 - 96;
+            return determine_priority(item_1);
         }
     }
     panic!("Nothing in rucksack");
 }
 
-fn score_part_2(group: &Vec<String>) -> u32 {
-
-    return 0;
+fn score_part_2(group: &[String]) -> u32 {
+    for item_0 in group[0].chars() {
+        for item_1 in group[1].chars() {
+            for item_2 in group[2].chars() {
+                if item_0 != item_1 {
+                    break;
+                }
+                if item_1 != item_2 {
+                    continue;
+                }
+                return determine_priority(item_1);
+            }
+        }
+    }
+    panic!("No matching item in group");
 }
